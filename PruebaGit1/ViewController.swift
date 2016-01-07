74785e723daeadf037a666ce78373caf16748b66
Henry Molina Gómez <henrymolinagomez@macbook-pro-de-henry-molina-gomez.local>
2016-01-07 18:13:28 -0500

    Initial Commit

---
+++ PruebaGit1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PruebaGit1
+//
+//  Created by Henry Molina Gómez on 7/01/16.
+//  Copyright © 2016 Henry Molina Gómez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
